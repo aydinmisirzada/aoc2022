7bff432f843963ceb99249f4e87f8655eced20d3
Aydin Misirzade <aydin.misirzade@team.wrike.com>
2022-12-01 14:33:30 +0100

    fix formatting, add typing, fix bugs

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,4 @@
-def star1(input_file):
+def star1(input_file: str) -> int:
     """Should return 24000 for sample data.
 
     >>> star1('day1_test_input.txt')
@@ -7,27 +7,30 @@ def star1(input_file):
     max_calories = 0
     elf_calories = 0
 
-    with open(input_file,'r') as f:
+    with open(input_file, 'r') as f:
         for line in f:
             if line == '\n':
                 max_calories = max(elf_calories, max_calories)
                 elf_calories = 0
             else:
                 elf_calories += int(line)
+        if elf_calories:
+            max_calories = max(elf_calories, max_calories)
 
     return max_calories
 
-def star2(input_file):
+
+def star2(input_file: str) -> int:
     """Should return 45000 for sample data.
 
     >>> star2('day1_test_input.txt')
     45000
     """
     calories = []
-    with open(input_file,'r') as f:
+    with open(input_file, 'r') as f:
         elf_calories = 0
         for line in f:
-            if not line or line == '\n':
+            if line == '\n':
                 calories.append(elf_calories)
                 elf_calories = 0
             else:
@@ -38,6 +41,7 @@ def star2(input_file):
     return sum(sorted(calories)[-3:])
 
 
-data = 'day1_input.txt'
-print('Result for the 1st star',star1(data))
-print('Result for the 2nd star',star2(data))
+if __name__ == "__main__":
+    data = 'day1_input.txt'
+    print('Result for the 1st star', star1(data))
+    print('Result for the 2nd star', star2(data))
