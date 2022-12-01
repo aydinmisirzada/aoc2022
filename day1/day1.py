3e46054ebbf066dd6cd623431faa793ac2f776a2
Aydin Misirzade <aydin.misirzade@team.wrike.com>
2022-12-01 13:35:00 +0100

    add unittests

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,9 @@
 def star1(input_file):
+    """Should return 24000 for sample data.
+
+    >>> star1('day1_test_input.txt')
+    24000
+    """
     max_calories = 0
     elf_calories = 0
 
@@ -10,22 +15,28 @@ def star1(input_file):
             else:
                 elf_calories += int(line)
 
-
     return max_calories
 
 def star2(input_file):
+    """Should return 45000 for sample data.
+
+    >>> star2('day1_test_input.txt')
+    45000
+    """
     calories = []
     with open(input_file,'r') as f:
         elf_calories = 0
         for line in f:
-            if line == '\n':
+            if not line or line == '\n':
                 calories.append(elf_calories)
                 elf_calories = 0
             else:
                 elf_calories += int(line)
-
+        if elf_calories:
+            calories.append(elf_calories)
 
     return sum(sorted(calories)[-3:])
+
 
 data = 'day1_input.txt'
 print('Result for the 1st star',star1(data))
